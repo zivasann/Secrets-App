79c773ab1db6662d1d3f73bfbdf55a88a84ad4e1
Diana Hydzik <hydzik.diana@gmail.com>
2026-03-11 19:31:34 +0000

    Update database connection for Render deployment

--- index.js
+++ index.js
@@ -33,11 +33,10 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 db.connect().catch((err) => {
